b067d79ae2074c4a2b195c57b9ce3e58d1ca8977
lingang <qq1812159202@163.com>
2024-01-14 10:53:28 +0800

    hashmap存入键值对

--- HashMap/hash_map.c
+++ HashMap/hash_map.c
@@ -2,9 +2,22 @@
 
 int main(void) {
     // 创建hashmap
-    int capacity = 10;
+    int capacity = 3;
     double load_factor = 0.70;
     HashMap *map = create_hashmap(&capacity, &load_factor);
+    // 存值
+    put(map, "name", "tom");
+    put(map, "age", "20");
+    put(map, "gender", "male");
+    // 取值
+    printf("%s\n", get(map, "name"));
+    // 删值
+
+
+
+
+
+
 
 
     // 销毁
@@ -38,35 +51,133 @@ HashMap* create_hashmap(const void* capacity_p, const void* load_factor_p) {
 }
 // 销毁HashMap
 void destroy_hashmap(HashMap *map) {
+    if (map == NULL) {
+        puts("error：destroy_hashmap()的参数map为NULL");
+        exit(-1);
+    }
     // 1. 销毁链表结点
-
-    // 2. 销毁桶
-
+    for (int i = 0; i < map->capacity; i++) {
+        Bucket *cur_bucket = map->buckets[i];
+        if (cur_bucket != NULL) {
+            MapNode *cur = cur_bucket->head;
+            while(cur != NULL) {
+                MapNode *temp = cur->next;
+                free(cur);
+                cur = temp;
+            }
+            // 2. 销毁桶
+            free(cur_bucket);
+        }
+    }
     // 3. 销毁map
-
+    free(map);
 }
 // ---------------- 基本操作 ----------------
 // 存入键值对
 V put(HashMap *map, K key, V val) {
-
-    return map->buckets[0]->head->value;
+    if (map == NULL) {
+        puts("error：put()的参数map为NULL");
+        exit(-1);
+    }
+    // 1. 计算key的hash值以及桶的位置idx
+    uint32_t hash_code = hash(key, strlen(key), map->hash_seed);
+    int idx = hash_code % map->capacity;
+    // 2. 判断idx位置的桶是否存在
+    if (map->buckets[idx] != NULL) {
+        // 2-1. idx位置的桶已存在，则判断idx桶中是否存在key值
+        MapNode *cur = map->buckets[idx]->head;
+        while (cur != NULL) {
+            if (strcmp(cur->key, key) == 0) {
+                // 2-1-1. 若idx桶中已存在key值则更新value值，并返回旧value值
+                V old_val = cur->value;
+                cur->value = val;
+                return old_val;
+            }
+            cur = cur->next;
+        }
+    }
+    // 3. 判断是否需要扩容
+    if (is_need_resize(map)) {
+        // 3-1. 若需扩容，则扩容，并重新计算key的hash值和桶位置idx
+        resize(map);
+        hash_code = hash(key, strlen(key), map->hash_seed);
+        idx = hash_code % map->capacity;
+    }
+    // 4. 重新判断idx位置的桶是否存在（在‘2.’虽然判断了一次，但有可能新的idx位置桶还未创建过
+    if (map->buckets[idx] == NULL) {
+        // 4-1. idx位置的桶不存在，则在idx位置创建桶，并在桶中存入hash值
+        Bucket *bucket = calloc(1, sizeof(Bucket));
+        if (bucket == NULL) {
+            puts("error：put()内分配内存失败");
+            exit(-1);
+        }
+        bucket->hash_code = hash_code;
+        map->buckets[idx] = bucket;
+    }
+    // 5. 在桶中插入这键值对（用头插 O(1)）
+    MapNode *new_node = calloc(1, sizeof(MapNode));
+    if (new_node == NULL) {
+        puts("error：put()内分配内存失败");
+        exit(-1);
+    }
+    new_node->key = key;
+    new_node->value = val;
+    new_node->next = map->buckets[idx]->head;
+    map->buckets[idx]->head = new_node;
+    // 6. 更新size 
+    map->size++;
+    return NULL;
 }
 // 查询键值对
 V get(const HashMap *map, K key) {
+    if (map == NULL) {
+        puts("error：get()的参数map为NULL");
+        exit(-1);
+    }
 
-    return map->buckets[0]->head->value;
+    return NULL;
 }
 // 删除键值对
 bool map_remove(HashMap *map) {
+    if (map == NULL) {
+        puts("error：map_remove()的参数map为NULL");
+        exit(-1);
+    }
     
+    return false;
+}
+// 键key在表中是否有对应的值
+bool contains(const HashMap *map, K key) {
+    if (map == NULL) {
+        puts("error：contains()的参数map为NULL");
+        exit(-1);
+    }
+
     return false;
 }
 // 判断是否为空
 bool is_empty(const HashMap *map) {
+    if (map == NULL) {
+        puts("error：is_empty()的参数map为NULL");
+        exit(-1);
+    }
 
     return false;
 }
 
+// ---------------- 其他操作 ----------------
+// 是否需要扩容
+static bool is_need_resize(const HashMap *map) {
+    double load_factor = map->size*1.0 / map->capacity;
+    if (load_factor > map->load_factor) {
+        return true;
+    }
+    return false;
+}
+// TODO：扩容
+static void resize(HashMap *map) {
+
+}
 
 // murmur_hash2 哈希函数
 static uint32_t hash(const void* key, int len, uint32_t seed) {
--- HashMap/hash_map.h
+++ HashMap/hash_map.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include <time.h>
 // 默认桶的个数(动态数组默认大小)
 #define DEFAULT_CAPACITY 8
@@ -22,7 +23,7 @@ typedef struct map_node {
 // 定义hashmap的桶结点
 typedef struct {
     MapNode *head; // 桶中链表的第一个节点
-    uint32_t hash; // 桶中键值对hash值
+    uint32_t hash_code; // 桶中键值对hash值
 } Bucket;
 
 // 定义hashmap结构体
@@ -45,10 +46,16 @@ V put(HashMap *map, K key, V val);
 V get(const HashMap *map, K key);
 // 删除键值对
 bool map_remove(HashMap *map);
+// 键key在表中是否有对应的值
+bool contains(const HashMap *map, K key);
 // 判断是否为空
 bool is_empty(const HashMap *map);
 
-
+// ---------------- 其他操作 ----------------
+// 是否需要扩容
+static bool is_need_resize(const HashMap *map);
+// 扩容
+static void resize(HashMap *map);
 // murmur_hash2 哈希函数
 static uint32_t hash(const void* key, int len, uint32_t seed);
 
